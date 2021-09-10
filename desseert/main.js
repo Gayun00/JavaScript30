64f58d3ef31925e2da7d67b820d7470d3e4dd633
Gayun Lee <gayunlee11@gmail.com>
2021-09-11 06:50:38 +0900

    fetch_json items data

--- desseert/main.js
+++ desseert/main.js
@@ -1,12 +1,35 @@
+const $imgSection = document.querySelector(".img-section");
+
 function loadItems() {
-  return fetch("data/data.json").then((response) => response.json());
+  return fetch("data/data.json")
+  .then((response) => response.json())
+  .then(json => json.items);
 }
 
 loadItems()
   .then((items) => {
     console.log(items)
+    displayItems(items)
   })
 
   .catch((items) => {
-    console.log(items)
+    console.log("catch")
   })
+
+// function displayItems(items) {
+//   console.log("ddddd")
+//   const html = items.map((item) => createHTMLString(item).join(""));
+//   console.log(html)
+// }
+
+// function createHTMLString() {
+//   return `
+//   <box class="item-container">
+//   <img class="item-img" src="${items.image}" >
+//   <div class="item-info">
+//     <span class="item-title">${items.title}</span>
+//     <span class="item-price">${items.price}</span>
+//   </div>
+// </box>
+//   `
+// }
