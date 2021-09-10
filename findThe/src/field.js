8e8a3348de0433173b9130586087090a3d3056f5
Gayun Lee <gayunlee11@gmail.com>
2021-09-10 15:53:40 +0900

    creating field class

---
+++ findThe/src/field.js
@@ -0,0 +1,54 @@
+'use strict';
+
+export default class Field {
+  constructor(item_count) {
+    this.item_count = 5;
+    this.$gameField = document.querySelector(".game-field");
+    this.$fieldRect = this.$gameField.getBoundingClientRect();
+    this.$gameField.addEventListener("click", this.onClick);
+  }
+
+  onClick(ev) {
+    const target = ev.target;
+    if (target.matches(".radio")) {
+      target.remove();
+      this.onItemClick && this.onItemClick('radio');
+    } else {
+    }
+      stopGame();
+    };
+
+  _addItem(className, count, imgPath) {
+    const x1 = 0;
+    const y1 = 0;
+    const x2 = this.$fieldRect.width - ITEM_SIZE;
+    const y2 = this.$fieldRect.height - ITEM_SIZE;
+
+    for(let i = 0; i < count; i++){
+      const $item = document.createElement("img");
+      $item.setAttribute("src", imgPath);
+      $item.setAttribute("class", className);
+      this.$gameField.appendChild($item);
+
+      $item.style.position = 'absolute'
+      const x = randomNumber(x1, x2);
+      const y = randomNumber(y1, y2);
+      $item.style.left = `${x}px`
+      $item.style.top = `${y}px`
+    }
+  }
+
+  init() {
+    this.$gameField.innerHTML = "";
+    _addItem('smiley', this.item_count, 'img/smiley.png');
+    _addItem('radio', this.item_count, 'img/radio.png');
+    _addItem('flower', this.item_count, 'img/flower.png');
+    _addItem('egg', this.item_count, 'img/egg.png');
+  }
+
+  setClickListener(onItemClick) {
+    this.onItemClick = onItemClick;
+  }
+
+
+}
--- findThe/src/main.js
+++ findThe/src/main.js
@@ -1,4 +1,5 @@
 
+import Field from "./field.js";
 import PopUp from "./popup.js";
 
 const ITEM_COUNT = 5;
@@ -14,17 +15,39 @@ const $startBtn = document.querySelector(".start-btn");
 const $timer = document.querySelector(".timer");
 const $score = document.querySelector(".score");
 
-const $gameField = document.querySelector(".game-field");
-const $fieldRect = $gameField.getBoundingClientRect();
 
 const gameFinishBanner = new PopUp();
 gameFinishBanner.setClickListener(() => {
   startGame();
 })
 
+const gameField = new Field();
+gameField.setClickListener(onItemClick);
+
+
 $startBtn.addEventListener("click", startOrStop);
 
-$gameField.addEventListener("click", clickItem);
+// gameField.addEventListener("click", clickItem);
+
+function onItemClick(ev) {
+  if (started === false) {
+    return;
+  }
+  // const target = ev.target;
+  if (item.matches(".radio")) {
+    SCORE++;
+    // target.remove();
+  } else {
+    SCORE--;
+    // console.log("dang...")
+  }
+  if (SCORE < 0 || TIME ===0) {
+    stopGame();
+  } else if (SCORE === ITEM_COUNT - 1) {
+    gameFinishBanner.showWithText('won!')
+  };
+  $score.innerText = `${SCORE}`;
+}
 
 function startOrStop() {
   if (!started) {
@@ -36,7 +59,7 @@ function startOrStop() {
 }
 
 function startGame() {
-    initGame();
+    gameField.init();
     showTimerAndScore();
     startTimer(TIME);
     hideIntro();
@@ -44,34 +67,6 @@ function startGame() {
 
 function hideIntro() {
   $intro.classList.add("intro-hide");
-}
-
-function initGame() {
-  $gameField.innerHTML = "";
-  addItem('smiley', ITEM_COUNT, 'img/smiley.png');
-  addItem('radio', ITEM_COUNT, 'img/radio.png');
-  addItem('flower', ITEM_COUNT, 'img/flower.png');
-  addItem('egg', ITEM_COUNT, 'img/egg.png');
-}
-
-function addItem(className, count, imgPath) {
-  const x1 = 0;
-  const y1 = 0;
-  const x2 = $fieldRect.width - ITEM_SIZE;
-  const y2 = $fieldRect.height - ITEM_SIZE;
-
-  for(let i = 0; i < count; i++){
-    const $item = document.createElement("img");
-    $item.setAttribute("src", imgPath);
-    $item.setAttribute("class", className);
-    $gameField.appendChild($item);
-
-    $item.style.position = 'absolute'
-    const x = randomNumber(x1, x2);
-    const y = randomNumber(y1, y2);
-    $item.style.left = `${x}px`
-    $item.style.top = `${y}px`
-  }
 }
 
 function randomNumber(min, max) {
@@ -96,25 +91,7 @@ function startTimer(time) {
   }, 1000);
 }
 
-function clickItem(ev) {
-  if (started === false) {
-    return;
-  }
-  const target = ev.target;
-  if (target.matches(".radio")) {
-    SCORE++;
-    target.remove();
-  } else {
-    SCORE--;
-    console.log("dang...")
-  }
-  if (SCORE < 0 || TIME ===0) {
-    stopGame();
-  } else if (SCORE === ITEM_COUNT - 1) {
-    gameFinishBanner.showWithText('won!')
-  };
-  $score.innerText = `${SCORE}`;
-}
+
 
 function stopGame() {
   gameFinishBanner.showWithText('replay?');
