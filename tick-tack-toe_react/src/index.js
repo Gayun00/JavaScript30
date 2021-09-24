a4418193a4ab8c1321ffed718ecb695e4d310cbe
Gayun Lee <gayunlee11@gmail.com>
2021-09-25 06:41:20 +0900

    mark X when clicked

--- tick-tack-toe_react/src/index.js
+++ tick-tack-toe_react/src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    //클래스에 생성자를 추가하여 state 초기화
+    super(props);
+    //하위 클래스의 생성자를 정의할 때 항상 super를 호출해야 한다.
+    //모든 리액트 컴포넌트 클래스는 생성자를 가질 때 super(props)호출 구문부터 시작해야 한다.
+    this.state = {
+      value: null,
+    }
+  }
   render() {
     return (
-      <button className="square" onClick={() => console.log('click')}>
-        {this.props.value}
+      <button className="square"
+        onClick={() => this.setState({value:'X'})}>
+        {this.state.value}
       </button>
     );
   }
