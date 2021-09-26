df24df837299a0d8622a25195764fd721e9378ea
Gayun Lee <gayunlee11@gmail.com>
2021-09-26 09:16:35 +0900

    Board: set handleClick(i)

--- tick-tack-toe/ver3/src/index.js
+++ tick-tack-toe/ver3/src/index.js
@@ -7,7 +7,7 @@ class Square extends React.Component {
 
   render() {
     return (
-      <button className="square" onClick={() =>this.props.onClick}>
+      <button className="square" onClick={() =>this.props.onClick()}>
         {this.props.value}
       </button>
     );
@@ -22,13 +22,15 @@ class Board extends React.Component {
     }
   }
   handleClick(i){
-    
+    const squares = this.state.squares.slice()
+    squares[i] = 'X';
+    this.setState({squares});
   }
 
   renderSquare(i) {
-    return <Square 
+    return <Square
       value={this.state.squares[i]}
-      onClick={this.state.handleClick(i)}
+      onClick={() => this.handleClick(i)}
     />;
   }
 
