6ecaa2c3d092e12132a3f438a69170870f6fc304
Gayun Lee <gayunlee11@gmail.com>
2021-10-02 07:54:47 +0900

    show thumbnails

--- youtube/youtube/src/component/video_item/video_item.jsx
+++ youtube/youtube/src/component/video_item/video_item.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import VideoList from '../video_list/video_list';
 
 const VideoItem = (props) => (
-      <h3>{props.video.snippet.title}</h3>
+      <li>
+        <img src={props.video.snippet.thumbnails.medium.url} alt="" />
+        
+      </li>
   );
 
 export default VideoItem;
--- youtube/youtube/src/component/video_list/video_list.jsx
+++ youtube/youtube/src/component/video_list/video_list.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import VideoItem from '../video_item/video_item';
 
 const VideoList = (props) => (
-      props.videos.map((video)=> <VideoItem video={video}/>)
+      <ul>
+        {props.videos.map((video)=> <VideoItem video={video}/>)}
+      </ul>
   );
 
 export default VideoList;
