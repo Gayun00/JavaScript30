013e49d7f247cdf4cf72087ff9875f5fdc27c57e
Gayun Lee <gayunlee11@gmail.com>
2021-10-03 18:21:43 +0900

    show title & subtitle

--- youtube/youtube/src/component/video_item/video_item.jsx
+++ youtube/youtube/src/component/video_item/video_item.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import VideoList from '../video_list/video_list';
+import styles from './video_item.module.css'
 
 const VideoItem = (props) => (
-      <li>
+      <li className={styles.item}>
         <img src={props.video.snippet.thumbnails.medium.url} alt="" />
-        
+        <h4 className={styles.title}>{props.video.snippet.title}</h4>
+        <h5>{props.video.snippet.channelTitle}</h5>
       </li>
   );
 
