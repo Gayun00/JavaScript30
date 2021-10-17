30779c9c66aed9d591e9645032ed9a6dcbd435ed
Gayun Lee <gayunlee11@gmail.com>
2021-10-18 00:29:59 +0900

    Main : banner --complete

--- new/delivery/src/component/main/main.jsx
+++ new/delivery/src/component/main/main.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './main.module.css';
 import {useHistory} from 'react-router';
+import { useEffect } from 'react/cjs/react.development';
 
 const Main = (props) => {
+  const [slideState, setSlideState] = useState(0);
+  const MAX_SLIDES = 3;
+  const slideRef = useRef();
   const history = useHistory();
+
+
   const goToOrder =() => {
     history.push('/order')
   }
-  
+
+  const handleBannerLeft =()=> {
+    console.log(slideState)
+    if(slideState === 0) {
+      setSlideState(MAX_SLIDES);
+    }else {
+      setSlideState(slideState - 1);
+    }
+  }
+
+  const handleBannerRight =() => {
+    console.log(slideState)
+    if(slideState >= MAX_SLIDES) {
+      setSlideState(0)
+    } else {
+      setSlideState(slideState + 1);
+    }
+  }
+
+  useEffect(()=> {
+    slideRef.current.style.transition ="all 0.5s ease-in-out";
+    slideRef.current.style.transform= `translateX(-${slideState}00%)`;
+  },[slideState])
+
+
+
   return (
   <div className={styles.container}>
     <header className={styles.header}>
@@ -16,13 +47,44 @@ const Main = (props) => {
      <div className={styles.header_content}>정보수정</div>
      <div className={styles.header_content}>고객센터</div>
     </header>
-    <nav>
-      <img className={styles.logo_main} src="./main_logo.png" />
-      <div className={styles.navbar_content}>서비스 안내</div>
-      <div className={styles.navbar_content}>배송대행</div>
-      <div className={styles.navbar_content}>고객센터</div>
+
+    <nav className={styles.nav}>
+      <img className={styles.logo_main} src="./main_logo_horizontal.png" />
+      <div className={styles.nav_container}>
+        <div className={styles.nav_content}>서비스 안내</div>
+        <div className={styles.nav_content}>배송대행</div>
+        <div className={styles.nav_content}>고객센터</div>
+        <button className={styles.menu_button}>B</button>
+      </div>
     </nav>
-    <div className={styles.banner}>Banner</div>
+
+
+    <div className={styles.banner} ref={slideRef}>
+      <div className={styles.banner_images}>
+        <img className={styles.banner_img}
+          src="./banner1.jpg"/>
+        <img className={styles.banner_img}
+          src="./banner2.jpg"/>
+        <img className={styles.banner_img}
+          src="./banner3.jpg"/>
+        <img className={styles.banner_img}
+          src="./banner4.jpg"/>
+      </div>
+    </div>
+
+      <div className={styles.banner_buttons}>
+        <button className={styles.banner_button}
+          onClick={handleBannerLeft}
+        > (
+        </button>
+
+        <button className={styles.banner_button}
+          onClick={handleBannerRight}
+        > )
+        </button>
+      </div>
+
+
     <div className={styles.buttons_main}>
       <button
         className={styles.order_button}
