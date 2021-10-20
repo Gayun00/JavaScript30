fb66d24e8a3c05fba8723219b55270dd09573f3d
Gayun Lee <gayunlee11@gmail.com>
2021-10-20 19:18:50 +0900

    profile button, nav contact : scroll to contact

--- website/main.js
+++ website/main.js
@@ -1,12 +1,29 @@
 const $navBar = document.querySelector(".nav-bar");
 const $navBarHeight = $navBar.getBoundingClientRect().height
-// console.log($navBarHeight)
+
+const $menuHome = document.querySelector(".menu--home");
+const $menuAbout = document.querySelector(".menu--about");
+const $menuSkills = document.querySelector(".menu--skills");
+const $menuMyWork = document.querySelector(".menu--my-work");
+const $menuContact = document.querySelector(".menu--contact");
+
+const $profileButton = document.querySelector(".profile__button");
+
+
+const $skills = document.querySelector(".skills");
+const $contact = document.querySelector(".contact");
+
 document.addEventListener("scroll", () => {
-  // console.log(window.scrollY)
   if(window.scrollY > $navBarHeight) {
-    // console.log("more than that")
     $navBar.classList.add("nav-bar-background")
   } else {
     $navBar.classList.remove("nav-bar-background")
   }
 })
+
+$menuContact.addEventListener("click",scrollToContact);
+function scrollToContact(){
+  $contact.scrollIntoView({behavior:"smooth"})
+}
+
+$profileButton.addEventListener("click", scrollToContact)
