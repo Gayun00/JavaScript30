c009ccca49870352544282b048eeac7abff1b682
Gayun Lee <gayunlee11@gmail.com>
2021-10-20 13:34:13 +0900

    audio play

---
+++ JavaScript30-master/JavaScript30-master/01 - JavaScript Drum Kit/main.js
@@ -0,0 +1,15 @@
+/**
+ * 어떻게 접근해야할까?
+ * 이벤트 위임으로 전체에 이벤트를 등록하되,
+ * class가 key일때만 반응한다
+ * 
+ * 그리고 ..클릭한 키의 data값을 읽어온다음
+ * 반환해보자.
+ */
+
+window.addEventListener('keydown', (ev)=> {
+  const $audio = document.querySelector(`audio[data-key="${ev.keyCode}"]`)
+  console.log($audio)
+  if(!$audio) return;
+  $audio.play();
+})
