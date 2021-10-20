90f34cc768beed6af4e333f1ca4bf5ac5c6d1efd
Gayun Lee <gayunlee11@gmail.com>
2021-10-20 13:36:32 +0900

    play audio with ever clicked --currentTIme

--- JavaScript30-master/JavaScript30-master/01 - JavaScript Drum Kit/main.js
+++ JavaScript30-master/JavaScript30-master/01 - JavaScript Drum Kit/main.js
@@ -9,7 +9,7 @@
 
 window.addEventListener('keydown', (ev)=> {
   const $audio = document.querySelector(`audio[data-key="${ev.keyCode}"]`)
-  console.log($audio)
   if(!$audio) return;
+  $audio.currentTime = 0;
   $audio.play();
 })
