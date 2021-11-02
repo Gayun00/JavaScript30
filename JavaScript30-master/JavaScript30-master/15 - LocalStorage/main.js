962e7bbe5ffaed674131eb4e6f06bbce828fb77b
Gayun Lee <gayunlee11@gmail.com>
2021-11-02 13:25:51 +0900

    update

--- JavaScript30-master/JavaScript30-master/15 - LocalStorage/main.js
+++ JavaScript30-master/JavaScript30-master/15 - LocalStorage/main.js
@@ -1,31 +1,10 @@
-const addItems = document.querySelector('.add-items');
-const itemsList = document.querySelector('.plates');
-const items = [];
+const $plates = document.querySelector('.plates');
+const $input = document.querySelector('input[name="item"]');
+const $addButton = document.querySelector('input[value="+ Add Item"]');
 
-addItems.addEventListener('submit', addItem);
+$addButton.addEventListener('click', handleSubmit);
 
-function addItem(ev) {
-  ev.preventDefault();
-  const text = (this.querySelector('[name=item]')).value;
-  const item = {
-    text,
-    done: false
-  };
-
-  items.push(item);
-  populateList(items, itemsList);
-  localStorage.setItem('items', JSON.stringify(items));
-  this.reset();
-}
-
-function populateList(plates = [], platesList) {
-  platesList.innerHTML = plates.map((plate, i) => {
-    return `
-    <li>
-      <input type="checkbox" data-index=${i} id="item${i}"
-        ${plate.done ? 'checked' : ''}/>
-      <label for="">${plate.text}</label>
-    </li>
-    `;
-  }).join('');
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log($input.value);
 }
