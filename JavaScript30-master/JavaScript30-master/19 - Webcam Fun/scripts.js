4bdbc9c8d1e32bd1a7aefe20a1714732a4706d87
Gayun Lee <gayunlee11@gmail.com>
2021-11-04 11:34:49 +0900

    getVIdeo from webCam, display on canvas

--- JavaScript30-master/JavaScript30-master/19 - Webcam Fun/scripts.js
+++ JavaScript30-master/JavaScript30-master/19 - Webcam Fun/scripts.js
@@ -3,3 +3,30 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+  .then(localMediaStream => {
+    console.log(localMediaStream);
+    video.srcObject = localMediaStream;
+    video.play();
+    paintToCanvas();
+  })
+  .catch(err => {
+    console.error(`OH NO!!`, err);
+  });
+}
+
+getVideo();
+
+
+function paintToCanvas() {
+  const width = 640;
+  const height = 480;
+  canvas.width = width;
+  canvas.height = height;
+  console.log(width,height)
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
