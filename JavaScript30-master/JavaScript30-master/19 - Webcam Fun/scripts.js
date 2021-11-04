2e431a2d571923a5591543faa96471559beef8a6
Gayun Lee <gayunlee11@gmail.com>
2021-11-04 14:11:11 +0900

    videoEffect : redEffect

--- JavaScript30-master/JavaScript30-master/19 - Webcam Fun/scripts.js
+++ JavaScript30-master/JavaScript30-master/19 - Webcam Fun/scripts.js
@@ -27,9 +27,11 @@ function paintToCanvas() {
   const height = 480;
   canvas.width = width;
   canvas.height = height;
-  console.log(width,height)
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    pixels = redEffect(pixels);
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
@@ -40,8 +42,17 @@ function takePhoto() {
   const data = canvas.toDataURL('image/jpeg');
   const link = document.createElement('a');
   link.href = data;
-  link.setAttribute('download', 'handsome');
-  link.textContent = 'Download Image';
+  link.setAttribute('download', 'downloadImg');
+  link.innerHTML = `<img src="${data}" alt="download Img"/>`
   strip.insertBefore(link, strip.firstChild);
 }
 
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i+0] = pixels.data[i+0] + 100; //red
+    pixels.data[i+1] = pixels.data[i+1] - 50; //green
+    pixels.data[i+2] = pixels.data[i+2] * 0.5; //blue
+  }
+
+  return pixels;
+}
