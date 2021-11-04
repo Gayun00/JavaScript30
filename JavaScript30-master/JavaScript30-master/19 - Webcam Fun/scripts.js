4dd526da343feb1ac86df7607878d03b805617ed
Gayun Lee <gayunlee11@gmail.com>
2021-11-04 15:34:49 +0900

    videoEffect: greenScreen

--- JavaScript30-master/JavaScript30-master/19 - Webcam Fun/scripts.js
+++ JavaScript30-master/JavaScript30-master/19 - Webcam Fun/scripts.js
@@ -30,7 +30,10 @@ function paintToCanvas() {
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
     let pixels = ctx.getImageData(0, 0, width, height);
-    pixels = redEffect(pixels);
+    // pixels = redEffect(pixels);
+    // pixels = rgbSplit(pixels);
+    pixels = greenScreen(pixels);
+    // ctx.globalAlpha = 0.1;
     ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
@@ -52,6 +55,43 @@ function redEffect(pixels) {
     pixels.data[i+0] = pixels.data[i+0] + 100; //red
     pixels.data[i+1] = pixels.data[i+1] - 50; //green
     pixels.data[i+2] = pixels.data[i+2] * 0.5; //blue
+  }
+
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; //red
+    pixels.data[i + 100] = pixels.data[i + 1]; //green
+    pixels.data[i - 550] = pixels.data[i + 2]; //blue
+  }
+
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      pixels.data[i + 3] = 0;
+      console.log(pixels.data)
+    }
   }
 
   return pixels;
