0c003eba21babc3b7d8883603b0747afcea86ddc
Gayun00 <gayunlee11@gmail.com>
2021-11-20 18:05:33 +0900

    display time countdown

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -2,39 +2,34 @@
 document.title = "yes!!!"
 
 let countdown;
-const timerDisplay = document.querySelector('.display__time-left');
-const endTime = document.querySelector('.display__end-time');
+const $timerDisplay = document.querySelector('.display__time-left');
+const $endTime = document.querySelector('.display__end-time');
+
+const $customForm = document.querySelector('#custom');
+const $customTimeInput = document.querySelector('input[name="minutes"]');
+const $timerButtons = document.querySelectorAll('.timer__button');
+
+$customForm.addEventListener('submit', handleCustomTimeInput)
+$timerButtons.forEach(($timerButton) => {
+    $timerButton.addEventListener('click',() => displayCountdown(
+        $timerButton.dataset.time
+    ))
+})
+
+
+function handleCustomTimeInput(e) {
+    e.preventDefault();
+    displayCountdown($customTimeInput.value);
+}
 
 function timer(seconds) {
     const now = Date.now();
-    const then = now + seconds * 1000;
-    displayTimeLeft(seconds);
-    displayEndTime(then);
+    const then = seconds + now * 1000;
 
-
-
-    countdown = setInterval(() => {
-        const secondsLeft = Math.round((then - Date.now()) / 1000);
-
-        if(secondsLeft <= 0) {
-            clearInterval(countdown);
-            return;
-        }
-        displayTimeLeft(secondsLeft);
-    }, 1000);
 }
 
-function displayTimeLeft(seconds) {
-    const minutes = Math.floor(seconds / 60);
-    const remainderSeconds = seconds % 60;
-    const display = `${minutes}: ${remainderSeconds < 10 ? 0 : ''}${remainderSeconds}`;
-    timerDisplay.textContent = display;
-    console.log(minutes);
-}
-
-function displayEndTime(timestamp) {
-    const end = new Date(timestamp);
-    const hour = end.getHours();
-    const minutes = end.getMinutes();
-    endTime.textContent = `Be Back At ${hour}:${minutes}`;
+function displayCountdown(seconds) {
+    const min = Math.floor(seconds / 60);
+    const secondsLeft = seconds % 60;
+    $timerDisplay.textContent = `${min} : ${secondsLeft}`;
 }
